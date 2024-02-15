3b967362be59946b1ab668cb256bab296b82c6ad
HimajaChowdary <chowdary.ku@northeastern.edu>
2024-02-14 21:53:40 -0500

    Adding changes for terraform and gcloud setup

---
+++ main.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.16.0"
+    }
+  }
+}
+
+provider "google" {
+  project     = var.project_id
+  region      = var.region
+  credentials = file(var.credentials_file)
+}
+
+resource "google_compute_network" "vpc_network" {
+  name                    = var.network_name
+  routing_mode            = "REGIONAL"  
+  auto_create_subnetworks = false
+}
+
+# Creating subnets
+resource "google_compute_subnetwork" "webapp_subnet" {
+  name          = var.web_subnet_name
+  ip_cidr_range = var.web_subnet_cidr
+  region        = var.region
+  network       = google_compute_network.vpc_network.id
+}
+
+resource "google_compute_subnetwork" "db_subnet" {
+  name          = var.db_subnet_name
+  ip_cidr_range = var.db_subnet_cidr
+  region        = var.region
+  network       = google_compute_network.vpc_network.id
+}
+
+# Add route for webapp subnet
+resource "google_compute_route" "webapp_route" {
+  name                  = "webapp-route"
+  network               = google_compute_network.vpc_network.self_link
+  dest_range            = var.route_dest_range
+  next_hop_gateway      = var.route_next_hop
+}
