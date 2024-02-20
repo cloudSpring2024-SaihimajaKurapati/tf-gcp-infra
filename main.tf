06d19d9df35e9ef82572d2835d3306ca92562f19
HimajaChowdary <chowdary.ku@northeastern.edu>
2024-02-20 16:37:33 -0500

    testing

--- main.tf
+++ main.tf
@@ -38,7 +38,41 @@ resource "google_compute_subnetwork" "db_subnet" {
 # Add route for webapp subnet
 resource "google_compute_route" "webapp_route" {
   name                  = "webapp-route"
-  network               = google_compute_network.vpc_network.self_link
+  networ               = google_compute_network.vpc_network.self_link
   dest_range            = var.route_dest_range
   next_hop_gateway      = var.route_next_hop
+}
+
+# Create a firewall rule to block traffic to the SSH port
+resource "google_compute_firewall" "block_ssh_port" {
+  name    = "block-ssh-port"
+  network = google_compute_network.vpc_network.name
+
+  deny {
+    protocol = "tcp"
+    ports    = ["22"]  # SSH port
+  }
+
+  source_ranges = ["0.0.0.0/0"]  # Allow traffic from any source
+}
+
+# Create a Compute Engine instance
+resource "google_compute_instance" "web_instance" {
+  name         = "web-instance"
+  machine_type = "e2-small"  
+  zone         = var.zone
+  tags         = ["ssh-restricted"]
+
+  boot_disk {
+    initialize_params {
+      image = "projects/cloudgcp-414104/global/images/custom-image-1708459507"  
+      type  = "pd-balanced"   
+      size  = 100              
+    }
+  }
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.webapp_subnet.self_link
+    access_config {}
+  }
 }
