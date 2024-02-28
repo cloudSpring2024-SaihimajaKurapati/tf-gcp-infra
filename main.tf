bda9468670c232561631fbb4293278ecae535982
HimajaChowdary <chowdary.ku@northeastern.edu>
2024-02-28 01:22:51 -0500

    added script for clopudsql privat ip connection

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "google" {
-  project     = var.project_id
+  project     = var.project_id  # Ensure var.project_id is correctly set
   region      = var.region
   credentials = file(var.credentials_file)
 }
@@ -20,12 +20,35 @@ resource "google_compute_network" "vpc_network" {
   delete_default_routes_on_create = true
 }
 
+resource "google_compute_global_address" "private_service_address" {
+  project               = var.project_id
+  name         = "private-service-address"
+  address_type = "INTERNAL"
+  purpose      = "VPC_PEERING"
+  prefix_length = 24
+  network      = google_compute_network.vpc_network.id
+
+    # Change this to an IP in the DB subnet range
+}
+
+resource "google_service_networking_connection" "private_service_forwarding_rule" {
+  # provider              = google-beta
+  # project               = var.project_id
+  # name                  = "private-service-forwarding-rule"
+  # target                = "all-apis"
+  network               = google_compute_network.vpc_network.name
+  service               = "servicenetworking.googleapis.com"    
+  reserved_peering_ranges = [google_compute_global_address.private_service_address.name]
+  # load_balancing_scheme = ""
+}
+
 # Creating subnets
 resource "google_compute_subnetwork" "webapp_subnet" {
   name          = var.web_subnet_name
   ip_cidr_range = var.web_subnet_cidr
   region        = var.region
   network       = google_compute_network.vpc_network.id
+  private_ip_google_access = true
 }
 
 resource "google_compute_subnetwork" "db_subnet" {
@@ -44,6 +67,60 @@ resource "google_compute_route" "webapp_route" {
 }
 
 
+
+resource "random_id" "db_name_suffix" {
+  byte_length = 4
+}
+
+resource "google_sql_database_instance" "instance" {
+  project            = var.project_id
+  name               = "cloud-database-instance"
+  region             = var.region
+  database_version   = "MYSQL_5_7"
+  deletion_protection = false
+
+  settings {
+    tier              = "db-f1-micro"
+    availability_type = "REGIONAL"
+    disk_type         = "pd-ssd"
+    disk_size         = 100
+
+    ip_configuration {
+      ipv4_enabled      = false
+      private_network   = google_compute_network.vpc_network.self_link
+    }
+
+    backup_configuration {
+      binary_log_enabled = true  # Enable binary logging
+      enabled            = true  # Enable automatic backups
+    }
+  }
+
+  depends_on = [
+    google_service_networking_connection.private_service_forwarding_rule
+  ]
+}
+
+
+resource "google_sql_database" "webapp" {
+  name     = "webapp"
+  instance = google_sql_database_instance.instance.name
+}
+
+resource "random_password" "password" {
+  length           = 16
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+
+resource "google_sql_user" "webapp" {
+  name     = "webapp"
+  instance = google_sql_database_instance.instance.name
+  
+  password = random_password.password.result
+}
+
+
 # Create a firewall rule to block traffic to the SSH port
 resource "google_compute_firewall" "block_ssh_port" {
   name    = "block-ssh-port"
@@ -57,15 +134,12 @@ resource "google_compute_firewall" "block_ssh_port" {
   source_ranges = ["0.0.0.0/0"]  # Allow traffic from any source
 }
 
-
 # Create a Compute Engine instance
 resource "google_compute_instance" "web_instance" {
   name         = "web-instance"
   machine_type = "e2-small"  
   zone         = var.zone
-  tags         = ["appliation-instance"]
-
-  
+  tags         = ["application-instance"]
 
   boot_disk {
     initialize_params {
@@ -75,25 +149,33 @@ resource "google_compute_instance" "web_instance" {
     }
   }
   
-
   network_interface {
     subnetwork = google_compute_subnetwork.webapp_subnet.self_link
     access_config {}
   }
+
+   metadata_startup_script = <<-EOF
+    #!/bin/bash
+    echo "DB_DIALECT=mysql" >> /opt/csye6225/.env
+    echo "DB_HOST=${google_sql_database_instance.instance.private_ip_address}" >> /opt/csye6225/.env
+    echo "DB_USERNAME=${google_sql_user.webapp.name}" >> /opt/csye6225/.env
+    echo "DB_PASSWORD=${google_sql_user.webapp.password}" >> /opt/csye6225/.env
+    echo "DB_NAME=${google_sql_database.webapp.name}" >> /opt/csye6225/.env
+    chmod 600 /opt/csye6225/.env
+  EOF
 }
+ 
 
 resource "google_compute_firewall" "allow_application_port" {
   name    = "allow-application-port"
   network = google_compute_network.vpc_network.name
   
-
   allow {
     protocol = "tcp"
     ports    = [var.application_port]  
   }
 
   source_ranges = ["0.0.0.0/0"]  # Allow traffic from any source
-  target_tags         = ["appliation-instance"]
-  
+  target_tags         = ["application-instance"]
 }
 
